68c263d8a5a670d0f0b696080aa833824fd21173
Conmano <conmanoman@gmail.com>
2019-10-13 00:03:29 -0700

    Update html_file_handler.js
    Finsihed basic code (no bug testing done).

--- html_file_handler.js
+++ html_file_handler.js
@@ -12,23 +12,34 @@ function deleteFile(input_array, file_name){
 
 }
 
-function writeHTML(input_array, out_file_name){
+function writeHTML(input_array, file_name){
   var text = "<body>";
+  var lists = true;
 
   for(int i = 0; i < input_array.length; i++){
 		if(input_array[i].tagName == "UL" && input_array[i].tagName == "OL"){
-
+      for(int j = i; lists == true; j++){
+        if(input_array[j].tagName != "LI"){
+          lists = false;
+          i = j;
+        }
+        else{
+          text += "<li>" + input_array[j].CharacterData.data + "</li>";
+        }
+      }
     }
+    text += "<" + input_array[i].toLowerCase() + ">" + input_array[i].CharacterData.data + "</" +
+      input_array[i].toLowerCase() + ">";
   }
 
-
+  text += "</body>";
 
 
   //The function to create a file needs the data so I'm going to implement it below this
   //Data will be the String name we use for the creation of the new file
 
 
-  var file = new Blob([data], {html: html});
+  var file = new Blob([text], {html: html});
   if(window.navigator.msSaveOrOpenBlob)
   	window.navigator.msSaveOrOpenBlob(file, file_name);
   else{
